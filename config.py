b72cd223e92635ddfb3d654b1500b5eacb3737eb
frlinh <ci_linh@outlook.com>
2022-09-11 21:56:38 -0700

    adding random forest optimization

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 api_key = ""
 
 db_password = 'YOUR_PASSWORD_HERE'
+
+AWS RDS Connection
+PGEND_POINT = 'gameanalysis.cjhwxnjx5697.us-west-1.rds.amazonaws.com' # End_point
+PGDATABASE_NAME = 'gameanalysis' # Database Name example: youtube_test_db
+PGUSER_NAME = 'postgres' # UserName
+PGPASSWORD = '5}eN^2g0rac3' # Password
